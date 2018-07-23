792eeba3436e2fc3d0626405e4f43433e198bf8e
blockchainbuzz <36054391+blockchainbuzz@users.noreply.github.com>
2018-07-23 21:43:18 +0200

    init commit

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import { configure } from '@storybook/react';
+import 'storybook-chromatic/storybook-addon';
+
+import '../src/index.css';
+
+const req = require.context('../src/components', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
